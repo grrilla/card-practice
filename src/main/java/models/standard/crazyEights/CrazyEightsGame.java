ab5aae454a5c1afb10165fda656409a54e10c83e
Michael Longauer <mike.longauer@gmail.com>
2019-03-04 10:58:08 -0500

    eights

---
+++ src/main/java/models/standard/crazyEights/CrazyEightsGame.java
@@ -0,0 +1,9 @@
+package models.standard.crazyEights;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrazyEightsGame {
+
+  private static final transient Logger LOG = LoggerFactory.getLogger(CrazyEightsGame.class);
+}
---
+++ src/test/java/models/standard/crazyEights/CrazyEightsGameTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author groupby
+ */
+public class CrazyEightsGameTest {
+
+}
